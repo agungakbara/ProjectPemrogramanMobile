77e4b2f76560b29bc5396446c2728f31963ac093
Reva Ekalia <reva_ekalia@teknokrat.ac.id>
2025-05-24 23:27:59 +0700

    Initial commit

---
+++ app/src/main/java/com/uti/project_pm/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.uti.project_pm
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageView>(R.id.smk)
+        button.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+        }
+    }
+}
---
+++ app/src/main/java/com/uti/project_pm/login.kt
@@ -0,0 +1,12 @@
+package com.uti.project_pm
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
